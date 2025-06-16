c5e09fc7899e33b0493fa2d6c5b933810176338c
sandeep <murarishettysandeep688@gmail.com>
2025-06-17 01:18:56 +0530

    index datas

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+
 import Catering from "./pages/Catering";
 import Restaurant from "./pages/Restaurant";
 import FoodCourt from "./pages/FoodCourt";
@@ -23,7 +23,7 @@ const App = () => (
           <Route path="/catering" element={<Catering />} />
           <Route path="/restaurant" element={<Restaurant />} />
           <Route path="/foodcourt" element={<FoodCourt />} />
-          <Route path="*" element={<NotFound />} />
+          
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
